689a4c654280a1776d4134ae19a6ed8f1cbea127
Adolfo Rosauer <jurosauer@itba.edu.ar>
2023-06-24 20:54:07 -0300

    remove_comillas

--- src/backend/code-generation/listUtils.c
+++ src/backend/code-generation/listUtils.c
@@ -39,3 +39,24 @@ void freeList( stringList list ){
 
     return;
 }
+
+// thanks chatGPT!
+char* remover_comillas_extremos(char* input) {
+    int length = strlen(input);
+    if(length < 2) {
+        return NULL; // Si el string es demasiado corto, retornamos NULL.
+    }
+
+    // Verificamos si la primera y última caracter son comillas (simples o dobles)
+    if((input[0] == '\"' && input[length-1] == '\"') || (input[0] == '\'' && input[length-1] == '\'')) {
+        char* new_str = malloc(length - 1); // Creamos una nueva cadena con espacio para la nueva cadena sin las comillas.
+        if(new_str == NULL) {
+            return NULL; // Si no se pudo asignar la memoria, retornamos NULL.
+        }
+        strncpy(new_str, input + 1, length - 2); // Copiamos la cadena original sin las comillas.
+        new_str[length - 2] = '\0'; // Aseguramos que la cadena es nula terminada.
+        return new_str;
+    } else {
+        return strdup(input); // Si la cadena original no comienza y termina con comillas, la retornamos tal cual.
+    }
+}
