e3482e9a453b6b74ee2aa6ce9f7b2ce76b1a1ad8
Adolfo Rosauer <jurosauer@itba.edu.ar>
2023-06-24 11:56:49 -0300

    generator v1

--- src/backend/code-generation/generator.c
+++ src/backend/code-generation/generator.c
@@ -1,10 +1,26 @@
 #include "../support/logger.h"
 #include "generator.h"
+#include "../semantic-analysis/abstract-syntax-tree.h"
 
 /**
  * Implementación de "generator.h".
  */
 
-void Generator(int result) {
-	LogInfo("El resultado de la expresion computada es: '%d'.", result);
+void Generator(Program * program) {
+	// Generator va a generar un archivo .sql
+	// nombre del archivo harcodeado
+	LogInfo("El resultado de la expresion computada es: '%d'.", program);
+
+	//recorro el arbol desde program y formo el correcto codigo sql
+	//casos por tipo de general
+
+	//insert
+
+	//delete
+
+	//check
+
+	//query
+
+
 }
