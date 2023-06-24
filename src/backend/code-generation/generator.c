f9c9a0bc620632c525653961f8febef87847059d
dlimachi <dlimachi@itba.edu.ar>
2023-06-24 13:15:01 -0300

    agregando funciones en generator

--- src/backend/code-generation/generator.c
+++ src/backend/code-generation/generator.c
@@ -13,18 +13,45 @@ void Generator(Program * program) {
 
 	//recorro el arbol desde program y formo el correcto codigo sql
 	//casos por tipo de general
-	switch (program->general->type)
-	{
-	default:
-		break;
+	switch (program->general->type) {
+        case(INSERT):
+            generateInsert(program->general->insertBody);
+            break;
+        case(DELETE):
+            generateDelete(program->general->deleteBody);
+            break;
+        case(QUERY):
+            generateQuery(program->general->queryBody);
+            break;
+        case(CREATE):
+            generateCreate(program->general->createBody);
+            break;
+        case(CHECK):
+            generateCheck(program->general->check);
+            break;
+	    default:
+		    break;
 	}
-	//insert
-
-	//delete
-
-	//check
-
-	//query
-
 
 }
+
+void generateInsert(InsertBody * insertBody) {
+
+}
+
+void generateDelete(DeleteBody * deleteBody) {
+
+}
+
+void generateQuery(QueryBody * queryBody) {
+
+}
+
+void generateCreate(CreateBody * createBody) {
+
+}
+
+void generateCheck(Check * check) {
+
+}
+
